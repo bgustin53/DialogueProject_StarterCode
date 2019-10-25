96bc183e0baf5c99e12b90a4f9b9c9c782e1ce78
bgustin53 <bgustin@srcs.org>
2019-10-25 14:13:15 -0700

    Initial commit

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+
+    }
+
+    public void startDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("isOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        displayNextSentence();
+    }
+
+    public void displayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            endDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            for (int i = 0; i < 3; i++)
+            {
+                yield return null;
+            }
+        }
+    }
+    
+    void endDialogue()
+    {
+        animator.SetBool("isOpen", false);
+    }
+}
